822961637effb04515c539e62d3842cd1b247444
YouYou <yuta_mori@outlook.jp>
2021-10-04 01:14:28 +0900

    profile set

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "aws" {
   region  = "ap-northeast-1"
-  role_arn = arn:aws:iam::152231080651:role/ExampleGithubRole
+  role_arn = "arn:aws:iam::152231080651:role/ExampleGithubRole"
 }
 
 resource "aws_instance" "app_server_yuta" {
